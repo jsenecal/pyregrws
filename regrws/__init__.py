23e30752fda508b98c7d503650348a98cfdc0442
Jonathan Senecal <jonathan.senecal@metrooptic.com>
2023-02-07 15:56:54 -0500

    Skip DistributionNotFound in coverage

--- regrws/__init__.py
+++ regrws/__init__.py
@@ -2,5 +2,5 @@ from pkg_resources import get_distribution, DistributionNotFound
 
 try:
     __version__ = get_distribution("pyregrws").version
-except DistributionNotFound:
+except DistributionNotFound: # pragma: no cover
     pass
